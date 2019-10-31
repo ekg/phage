fe690592a5b00c88516d9836f0d6ea647a68ae57
Erik Garrison <erik.garrison@gmail.com>
2019-10-31 22:55:33 +0100

    updated analysis

--- scripts/run_shasta.sh
+++ scripts/run_shasta.sh
@@ -3,10 +3,25 @@
 x=$1
 input=$2
 
-time shasta --threads 32 --Align.minAlignedMarkerCount 10 --Reads.minReadLength 2500 --MarkerGraph.maxCoverage 1000000 --MarkerGraph.highCoverageThreshold 1000000 --ReadGraph.minComponentSize 10 --MinHash.minHashIterationCount 100 --MarkerGraph.pruneIterationCount 2  --input $input --assemblyDirectory $x 
+time shasta --threads 32 \
+     --Align.minAlignedMarkerCount 50 \
+     --Reads.minReadLength 2000 \
+     --Kmers.probability 0.3 \
+     --MarkerGraph.minCoverage 100 \
+     --MarkerGraph.maxCoverage 1000000 \
+     --MarkerGraph.highCoverageThreshold 1000000 \
+     --ReadGraph.maxAlignmentCount 10 \
+     --ReadGraph.minComponentSize 100 \
+     --MinHash.minHashIterationCount 20 \
+     --MarkerGraph.pruneIterationCount 6 \
+     --input $input \
+     --assemblyDirectory $x
+#--memoryMode filesystem --memoryBacking 2M \
+#shasta --command cleanupBinaryData
 
 # calculate coverage
-time minimap2 -t 32 -ax map-ont -r2k $x/Assembly.fasta $input | sambamba view -f bam -S /dev/stdin >$x.unsrt.bam \
+time minimap2 -t 32 -ax map-ont -r2k $x/Assembly.fasta $input \
+    | sambamba view -f bam -S /dev/stdin >$x.unsrt.bam \
     && sambamba sort -o $x.bam $x.unsrt.bam \
     && rm -f $x.unsrt.bam{,.bai} \
     && samtools coverage $x.bam >$x.bam.coverage \
