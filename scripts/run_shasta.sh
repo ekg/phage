20852cec67c82bd01d8d13ba97cdb433189de6fd
Erik Garrison <erik.garrison@gmail.com>
2019-11-02 15:49:25 +0100

    getting a single component and lots of alternate seqs

--- scripts/run_shasta.sh
+++ scripts/run_shasta.sh
@@ -5,19 +5,19 @@ input=$2
 
 time shasta --threads 32 \
     --Align.minAlignedMarkerCount 10 \
-    --Reads.minReadLength 1000 \
+    --Reads.minReadLength 3000 \
     --Kmers.k 10 \
     --Kmers.probability 0.1 \
     --MinHash.maxBucketSize 100 \
     --MarkerGraph.minCoverage 100 \
     --MarkerGraph.maxCoverage 1000000 \
     --MarkerGraph.lowCoverageThreshold 10 \
-    --MarkerGraph.highCoverageThreshold 1000 \
+    --MarkerGraph.highCoverageThreshold 100 \
     --MarkerGraph.edgeMarkerSkipThreshold 100 \
     --ReadGraph.maxAlignmentCount 6 \
     --ReadGraph.minComponentSize 10 \
     --MinHash.minHashIterationCount 20 \
-    --MarkerGraph.pruneIterationCount 6 \
+    --MarkerGraph.pruneIterationCount 2 \
     --input $input \
     --assemblyDirectory $x
 
