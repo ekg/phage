da315cdf4cab7520cedf674811aeec1a25ca8766
Erik Garrison <erik.garrison@gmail.com>
2020-01-15 11:17:56 +0100

    seqwish graph crushing recipe

---
+++ scripts/seqwish_assembly.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+f=$1
+b=$(basename $f .gfa)
+g=$b.gimbry
+
+gimbricate -g $f -f $g.fa -p $g.paf >$g.gfa
+minimap2 -c -X $g.fa $g.fa >$g.self.paf
+cat $g.paf $g.self.paf >$g.all.paf
+seqwish -s $g.fa -p $g.all.paf -g $b.seqwish.gfa
