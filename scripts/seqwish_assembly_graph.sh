9f7cf6d1fab92e60159de9922dfd773470450438
Erik Garrison <erik.garrison@gmail.com>
2020-01-15 11:48:58 +0100

    smooth the assembly squish
    But this doesn't work quite right because we can't read a GFA as input
    is seqwish!

--- scripts/seqwish_assembly_graph.sh
+++ scripts/seqwish_assembly_graph.sh
@@ -4,7 +4,7 @@ f=$1
 b=$(dirname $f)/$(basename $f .gfa)
 g=$b.gimbry
 
-gimbricate -g $f -f $g.fa -p $g.paf >$g.gfa
+gimbricate -g $f -f $g.fa -p $g.paf -c 100 >$g.gfa
 minimap2 -c -X $g.fa $g.fa >$g.self.paf
 cat $g.paf $g.self.paf >$g.all.paf
-seqwish -s $g.fa -p $g.all.paf -g $b.seqwish.gfa
+seqwish -s $g.fa -p $g.all.paf -k 8 -g $b.seqwish.gfa
