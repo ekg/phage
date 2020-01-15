04f3cff1abf9cc974e75f22675321297d1e803b2
Erik Garrison <erik.garrison@gmail.com>
2020-01-15 13:00:53 +0100

    add odgi prep script

---
+++ scripts/odgi_prep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+f=$1
+b=$(dirname $f)/$(basename $f .gfa)
+
+odgi build -g $f -o - | odgi sort -p mbn -N 64 -E 0.1 -M -i - -o $b.odgi
+odgi view -i $b.odgi -g >$b.odgi.gfa
+odgi viz -i $b.odgi -o $b.odgi.png -R -P 3 -x 8000 -y 400
+
+
+
+
+
+
