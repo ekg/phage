a451a90d72a5a9978127e2ade4f616bbf5598d2d
Erik Garrison <erik.garrison@gmail.com>
2020-01-20 17:55:36 +0100

    update plotter

--- scripts/plot_gaffy_matrix.sh
+++ scripts/plot_gaffy_matrix.sh
@@ -16,11 +16,21 @@ output=args[3]
 
 x <- read.delim(input)
 x <- subset(x, node.count > 10) # only keep apparently informative reads
-y <- sample_n(x, ifelse(nrow(x) > keep_num, keep_num, nrow(x)))
+if (nrow(x) <= keep_num) {
+   y <- x
+} else {
+   y <- sample_n(x, keep_num)
+}
 y.matrix <- y[ , !names(y) %in% c("aln.name","query.length","node.count")]
 y.dist <- dist(y.matrix)
 y.tree <- nj(y.dist)
 y.hclust <- hclust(y.dist)
 #plot(y.hclust)
 ggplot(y.tree) + geom_tree()
-ggsave(output, height=8, width=5)
+ggsave(paste(output, "tree.pdf", sep="."), height=8, width=5)
+y.pca <- prcomp(y.matrix); ggplot(y.pca$x, aes(x=PC1, y=PC2)) + geom_point()
+ggsave(paste(output, "pca.PC1.PC2.pdf", sep="."), height=8, width=8)
+y.pca <- prcomp(y.matrix); ggplot(y.pca$x, aes(x=PC2, y=PC3)) + geom_point()
+ggsave(paste(output, "pca.PC2.PC3.pdf", sep="."), height=8, width=8)
+y.pca <- prcomp(y.matrix); ggplot(y.pca$x, aes(x=PC3, y=PC4)) + geom_point()
+ggsave(paste(output, "pca.PC3.PC4.pdf", sep="."), height=8, width=8)
