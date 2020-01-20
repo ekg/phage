559fbbb5b9b77539a151a8d6ecec29f054bc8575
Erik Garrison <erik.garrison@gmail.com>
2020-01-20 06:42:43 -0600

    updates

--- scripts/odgi_prep.sh
+++ scripts/odgi_prep.sh
@@ -7,7 +7,8 @@ odgi build -g $f -o - | odgi sort -p mbn -N 64 -E 0.1 -M -i - -o $b.odgi
 odgi view -i $b.odgi -g >$b.odgi.gfa
 odgi viz -i $b.odgi -o $b.odgi.png -R -P 3 -x 8000 -y 400
 
-
+vg view -Fv $b.odgi.gfa >$b.odgi.vg
+vg index -x $b.odgi.xg $b.odgi.vg
 
 
 
