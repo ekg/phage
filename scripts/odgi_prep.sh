8c46fc582fc637295891c5b07b62a0939e861dd5
Erik Garrison <erik.garrison@gmail.com>
2020-01-22 12:12:58 +0100

    make names in odgi prep consistent

--- scripts/odgi_prep.sh
+++ scripts/odgi_prep.sh
@@ -9,8 +9,8 @@ odgi viz -i $b.odgi -o $b.odgi.png -R -P 3 -x 8000 -y 400
 odgi break -i $b.odgi -o $b.break.odgi -c 100 -s 100
 odgi view -i $b.break.odgi -g >$b.break.odgi.gfa
 
-vg view -Fv $b.break.odgi.gfa >$b.vg
-vg index -x $b.xg $b.vg
+vg view -Fv $b.break.odgi.gfa >$b.break.odgi.vg
+vg index -x $b.break.odgi.xg $b.break.odgi.vg
 
 
 
