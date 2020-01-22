05b747335ef3d49b3815c2df1d2bffdd33a1b55d
Erik Garrison <erik.garrison@gmail.com>
2020-01-22 15:20:39 +0100

    try harder to break little cycles

--- scripts/odgi_prep.sh
+++ scripts/odgi_prep.sh
@@ -7,7 +7,10 @@ odgi build -g $f -o - | odgi sort -p bSnSnS -t 4 -K -M -i - -o $b.odgi
 odgi view -i $b.odgi -g >$b.odgi.gfa
 odgi viz -i $b.odgi -o $b.odgi.png -P 3 -x 8000 -y 400
 odgi viz -i $b.odgi -o $b.odgi.R.png -R -P 3 -x 8000 -y 400
-odgi break -i $b.odgi -o $b.break.odgi -c 100 -s 100
+odgi break -i $b.odgi -o - -c 1000 -s 1000 \
+    | odgi break -i $b.odgi -o - -c 1000 -s 1000 \
+    | odgi break -i $b.odgi -o - -c 1000 -s 1000 \
+    | odgi break -i $b.odgi -o - -c 1000 -s 1000 >$b.break.odgi
 odgi view -i $b.break.odgi -g >$b.break.odgi.gfa
 
 vg view -Fv $b.break.odgi.gfa >$b.break.odgi.vg
--- scripts/plot_gaffy_matrix_P1-P10.sh
+++ scripts/plot_gaffy_matrix_P1-P10.sh
@@ -36,6 +36,7 @@ dev.off()
 ggtree(y.tree) %<+% data.frame(node=1:nrow(y.tree$edge), group.name=factor(c(as.character(y$group.name),rep("internal",nrow(y.tree$edge)-nrow(y))), levels=c(levels(y$group.name),"internal") )) + aes(color=group.name) + geom_tree() + scale_color_manual("passage",values=c(rainbow(13)[0:10], 'black'))
 ggsave(paste(output, "ggtree.passage.pdf", sep="."), height=40, width=9)
 
+# takes forever
 #ggtree(y.tree, layout="unrooted") %<+% data.frame(node=1:nrow(y.tree$edge), group.name=factor(c(as.character(y$group.name),rep("internal",nrow(y.tree$edge)-nrow(y))), levels=c(levels(y$group.name),"internal") )) + aes(color=group.name) + geom_tree() + scale_color_manual("passage",values=c(rainbow(13)[0:10], 'black'))
 #ggsave(paste(output, "ggtree.passage.unrooted.pdf", sep="."), height=40, width=9)
 
@@ -55,12 +56,13 @@ plotnj(y.tree, X.class=as.numeric(y$group.name), type='u', main='phylogeny of 5-
 dev.off()
 
 y.pca <- prcomp(y.matrix)
-y.pca$group.name <- y$group.name
-ggplot(y.pca$x, aes(x=PC1, y=PC2, color=group.name)) + geom_point() + scale_color_manual("passage",values=c(rainbow(13)[0:10], 'black'))
+y.pca.df <- as.data.frame(y.pca$x)
+y.pca.df$group.name <- y$group.name
+ggplot(y.pca.df, aes(x=PC1, y=PC2, color=group.name)) + geom_point() + scale_color_manual("passage",values=c(rainbow(13)[0:10], 'black'))
 ggsave(paste(output, "pca.PC1.PC2.pdf", sep="."), height=8, width=9)
-ggplot(y.pca$x, aes(x=PC2, y=PC3, color=group.name)) + geom_point() + scale_color_manual("passage",values=c(rainbow(13)[0:10], 'black'))
+ggplot(y.pca.df, aes(x=PC2, y=PC3, color=group.name)) + geom_point() + scale_color_manual("passage",values=c(rainbow(13)[0:10], 'black'))
 ggsave(paste(output, "pca.PC2.PC3.pdf", sep="."), height=8, width=9)
-ggplot(y.pca$x, aes(x=PC3, y=PC4, color=group.name)) + geom_point() + scale_color_manual("passage",values=c(rainbow(13)[0:10], 'black'))
+ggplot(y.pca.df, aes(x=PC3, y=PC4, color=group.name)) + geom_point() + scale_color_manual("passage",values=c(rainbow(13)[0:10], 'black'))
 ggsave(paste(output, "pca.PC3.PC4.pdf", sep="."), height=8, width=9)
-ggplot(y.pca$x, aes(x=PC4, y=PC5, color=group.name)) + geom_point() + scale_color_manual("passage",values=c(rainbow(13)[0:10], 'black'))
+ggplot(y.pca.df, aes(x=PC4, y=PC5, color=group.name)) + geom_point() + scale_color_manual("passage",values=c(rainbow(13)[0:10], 'black'))
 ggsave(paste(output, "pca.PC4.PC5.pdf", sep="."), height=8, width=9)
