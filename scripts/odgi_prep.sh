66305bd74e4529aabcc7baae3a4c8901df446ba7
Erik Garrison <erik.garrison@gmail.com>
2020-01-22 12:06:52 +0100

    correct typo in odgi sort pipeline

--- scripts/odgi_prep.sh
+++ scripts/odgi_prep.sh
@@ -3,7 +3,7 @@
 f=$1
 b=$(dirname $f)/$(basename $f .gfa)
 
-odgi build -g $f -o - | odgi sort -p -p bSnSnS -t 4 -K -M -i - -o $b.odgi
+odgi build -g $f -o - | odgi sort -p bSnSnS -t 4 -K -M -i - -o $b.odgi
 odgi view -i $b.odgi -g >$b.odgi.gfa
 odgi viz -i $b.odgi -o $b.odgi.png -R -P 3 -x 8000 -y 400
 odgi break -i $b.odgi -o $b.break.odgi -c 100 -s 100
