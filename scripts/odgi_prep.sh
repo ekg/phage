3a01be4af47755ab3dca1df6cbd4e99a92c4488e
Erik Garrison <erik.garrison@gmail.com>
2020-01-22 12:02:51 +0100

    break graph to make graphaligner happy

--- scripts/odgi_prep.sh
+++ scripts/odgi_prep.sh
@@ -3,12 +3,14 @@
 f=$1
 b=$(dirname $f)/$(basename $f .gfa)
 
-odgi build -g $f -o - | odgi sort -p mbn -N 64 -E 0.1 -M -i - -o $b.odgi
+odgi build -g $f -o - | odgi sort -p -p bSnSnS -t 4 -K -M -i - -o $b.odgi
 odgi view -i $b.odgi -g >$b.odgi.gfa
 odgi viz -i $b.odgi -o $b.odgi.png -R -P 3 -x 8000 -y 400
+odgi break -i $b.odgi -o $b.break.odgi -c 100 -s 100
+odgi view -i $b.break.odgi -g >$b.break.odgi.gfa
 
-vg view -Fv $b.odgi.gfa >$b.odgi.vg
-vg index -x $b.odgi.xg $b.odgi.vg
+vg view -Fv $b.break.odgi.gfa >$b.vg
+vg index -x $b.xg $b.vg
 
 
 
